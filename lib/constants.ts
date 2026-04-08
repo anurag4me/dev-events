59c1c4940c3032f5d333a47a4f1bc118555f411a
anurag4me <anuragmishra5033@gmail.com>
2026-04-09 01:16:14 +0530

    First Commit

---
+++ lib/constants.ts
@@ -0,0 +1,52 @@
+const events = [
+  {
+    title: "React Conf 2026",
+    image: "/images/event1.png",
+    slug: "react-conf-2026",
+    location: "Las Vegas, NV",
+    date: "June 15-17, 2026",
+    time: "9:00 AM - 6:00 PM",
+  },
+  {
+    title: "JSConf EU 2026",
+    image: "/images/event2.png",
+    slug: "jsconf-eu-2026",
+    location: "Berlin, Germany",
+    date: "May 20-22, 2026",
+    time: "10:00 AM - 7:00 PM",
+  },
+  {
+    title: "Google I/O 2026",
+    image: "/images/event3.png",
+    slug: "google-io-2026",
+    location: "Mountain View, CA",
+    date: "July 8-10, 2026",
+    time: "8:00 AM - 5:00 PM",
+  },
+  {
+    title: "PyCon US 2026",
+    image: "/images/event4.png",
+    slug: "pycon-us-2026",
+    location: "Pittsburgh, PA",
+    date: "May 12-20, 2026",
+    time: "9:00 AM - 6:00 PM",
+  },
+  {
+    title: "Hackathon Global 2026",
+    image: "/images/event5.png",
+    slug: "hackathon-global-2026",
+    location: "San Francisco, CA",
+    date: "August 5-7, 2026",
+    time: "24 hours",
+  },
+  {
+    title: "DevOps Days 2026",
+    image: "/images/event6.png",
+    slug: "devops-days-2026",
+    location: "Seattle, WA",
+    date: "April 25-27, 2026",
+    time: "9:00 AM - 5:00 PM",
+  },
+];
+
+export default events;
